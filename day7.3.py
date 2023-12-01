cec96274f5c506f084190766837eb0d85011c805
Heshan Vichalya Perera <heshanp.22@cse.mrt.ac.lk>
2023-12-01 12:27:30 +0530

    intial commit

---
+++ day7.3.py
@@ -0,0 +1,12 @@
+class rect:
+	def __init__(self,l,w):
+		self.length=l
+		self.width=w
+		self.area=l*w
+	def show(self):
+		print(self.length,self.width)
+		print("Area is : ",self.area)
+a=int(input("Enter Length: "))
+b=int(input("Enter Width : "))
+R=rect(a,b)
+R.show()
